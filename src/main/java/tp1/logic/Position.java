9506c685f99bad8f79b79a57420244f46c4e1ce6
NICOLE VERGARA <nicoleav@ucm.es>
2025-09-29 17:15:48 +0200

    Position Constructor

--- src/main/java/tp1/logic/Position.java
+++ src/main/java/tp1/logic/Position.java
@@ -11,5 +11,36 @@ public class Position {
 	private int row;
 
 	//TODO fill your code
+	//Constructor
+	public Position(int col, int row){
+		this.col = col;
+		this.row = row;
+	}
+	//Metodos (que puede hacer)
+	//Getters
+	public int getCol(){
+		return col;
+	}
 
+	public int getRow(){
+		return row;
+	}
+	
+	public void setCol(int col){
+		this.col = col;
+	}
+	public void setRow(int row){
+		this.row = row;
+	}
+	//Metodo para moverse: devuelve una nueva posicion, Posicion actual + desplazamiento
+	public Position move(int dcol,int drow){
+		return new Position(col + dcol, row + drow);
+	}
+
+	//Comprobar si la posicion es valida
+
+	public boolean isValidPosition(){
+		return col >= 0 && col < Game.DIM_X &&
+				row >= 0 && row < Game.DIM_Y;
+	}
 }
